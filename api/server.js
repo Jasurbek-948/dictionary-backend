d3afa469d9cdd131167eebe1f0e2ced9e5eae17c
Jasurbek-948 <uz.web.coder@gmail.com>
2025-08-20 18:09:11 +0500

    ishlashi kere

--- api/server.js
+++ api/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
-const dictionaryRoutes = require('./routes/dictionaryRoutes.js');
-const authRoutes = require('./routes/authRoutes.js');
-const { connectDB } = require('./config/db.js');
-const errorHandler = require('./middleware/errorHandler.js');
+const dictionaryRoutes = require('../routes/dictionaryRoutes.js');
+const authRoutes = require('../routes/authRoutes.js');
+const { connectDB } = require('../config/db.js');
+const errorHandler = require('../middleware/errorHandler.js');
 require('dotenv').config();
 
 const app = express();
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 app.use('/api/dictionary', dictionaryRoutes);
 app.use('/api/auth', authRoutes);
-app.use('/api/progress', require('./routes/progressRoutes.js'));
+app.use('/api/progress', require('../routes/progressRoutes.js'));
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
